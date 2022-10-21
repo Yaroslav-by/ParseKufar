9bd46a253144b5c5137fb013fdd3b6ef90a8c6ef
Yaroslav <asgen-yaroslav@mail.ru>
2022-10-21 23:11:31 +0300

    Finally, it can parse Kufar

--- src/gui/GUIParsing.java
+++ src/gui/GUIParsing.java
@@ -29,6 +29,7 @@ import javax.swing.table.DefaultTableModel;
 import parsing.ParseKufar;
 
 import java.awt.Component;
+import javax.swing.JTextArea;
 
 public class GUIParsing extends JFrame {
 
@@ -87,138 +88,156 @@ public class GUIParsing extends JFrame {
 		JComboBox<String> categoryComboBox = new JComboBox<String>();
 		categoryComboBox.setFont(new Font("Times New Roman", Font.PLAIN, 16));
 		categoryComboBox.setFocusable(false);
-		categoryComboBox.setModel(new DefaultComboBoxModel(new String[] {"\u041D\u0435 \u0432\u044B\u0431\u0440\u0430\u043D\u043E", "\u0411\u044B\u0442\u043E\u0432\u0430\u044F \u0442\u0435\u0445\u043D\u0438\u043A\u0430", "\u041A\u043E\u043C\u043F\u044C\u044E\u0442\u0435\u0440\u043D\u0430\u044F \u0442\u0435\u0445\u043D\u0438\u043A\u0430", "\u0422\u0435\u043B\u0435\u0444\u043E\u043D\u044B \u0438 \u043F\u043B\u0430\u043D\u0448\u0435\u0442\u044B", "\u042D\u043B\u0435\u043A\u0442\u0440\u043E\u043D\u0438\u043A\u0430", "\u0416\u0435\u043D\u0441\u043A\u0438\u0439 \u0433\u0430\u0440\u0434\u0435\u0440\u043E\u0431", "\u041C\u0443\u0436\u0441\u043A\u043E\u0439 \u0433\u0430\u0440\u0434\u0435\u0440\u043E\u0431", "\u041A\u0440\u0430\u0441\u043E\u0442\u0430 \u0438 \u0437\u0434\u043E\u0440\u043E\u0432\u044C\u0435", "\u0412\u0441\u0451 \u0434\u043B\u044F \u0434\u0435\u0442\u0435\u0439 \u0438 \u043C\u0430\u043C", "\u041C\u0435\u0431\u0435\u043B\u044C", "\u0412\u0441\u0435 \u0434\u043B\u044F \u0434\u043E\u043C\u0430", "\u0420\u0435\u043C\u043E\u043D\u0442 \u0438 \u0441\u0442\u0440\u043E\u0439\u043A\u0430", "\u0421\u0430\u0434 \u0438 \u043E\u0433\u043E\u0440\u043E\u0434", "\u0425\u043E\u0431\u0431\u0438, \u0441\u043F\u043E\u0440\u0442 \u0438 \u0442\u0443\u0440\u0438\u0437\u043C", "\u0421\u0432\u0430\u0434\u044C\u0431\u0430 \u0438 \u043F\u0440\u0430\u0437\u0434\u043D\u0438\u043A\u0438", "\u0416\u0438\u0432\u043E\u0442\u043D\u044B\u0435", "\u0413\u043E\u0442\u043E\u0432\u044B\u0439 \u0431\u0438\u0437\u043D\u0435\u0441 \u0438 \u043E\u0431\u043E\u0440\u0443\u0434\u043E\u0432\u0430\u043D\u0438\u0435", "\u0420\u0430\u0431\u043E\u0442\u0430", "\u0423\u0441\u043B\u0443\u0433\u0438", "\u041F\u0440\u043E\u0447\u0435\u0435"}));
+		categoryComboBox.setModel(new DefaultComboBoxModel<String>(new String[] {"\u041D\u0435 \u0432\u044B\u0431\u0440\u0430\u043D\u043E", "\u0411\u044B\u0442\u043E\u0432\u0430\u044F \u0442\u0435\u0445\u043D\u0438\u043A\u0430", "\u041A\u043E\u043C\u043F\u044C\u044E\u0442\u0435\u0440\u043D\u0430\u044F \u0442\u0435\u0445\u043D\u0438\u043A\u0430", "\u0422\u0435\u043B\u0435\u0444\u043E\u043D\u044B \u0438 \u043F\u043B\u0430\u043D\u0448\u0435\u0442\u044B", "\u042D\u043B\u0435\u043A\u0442\u0440\u043E\u043D\u0438\u043A\u0430", "\u0416\u0435\u043D\u0441\u043A\u0438\u0439 \u0433\u0430\u0440\u0434\u0435\u0440\u043E\u0431", "\u041C\u0443\u0436\u0441\u043A\u043E\u0439 \u0433\u0430\u0440\u0434\u0435\u0440\u043E\u0431", "\u041A\u0440\u0430\u0441\u043E\u0442\u0430 \u0438 \u0437\u0434\u043E\u0440\u043E\u0432\u044C\u0435", "\u0412\u0441\u0451 \u0434\u043B\u044F \u0434\u0435\u0442\u0435\u0439 \u0438 \u043C\u0430\u043C", "\u041C\u0435\u0431\u0435\u043B\u044C", "\u0412\u0441\u0435 \u0434\u043B\u044F \u0434\u043E\u043C\u0430", "\u0420\u0435\u043C\u043E\u043D\u0442 \u0438 \u0441\u0442\u0440\u043E\u0439\u043A\u0430", "\u0421\u0430\u0434 \u0438 \u043E\u0433\u043E\u0440\u043E\u0434", "\u0425\u043E\u0431\u0431\u0438, \u0441\u043F\u043E\u0440\u0442 \u0438 \u0442\u0443\u0440\u0438\u0437\u043C", "\u0421\u0432\u0430\u0434\u044C\u0431\u0430 \u0438 \u043F\u0440\u0430\u0437\u0434\u043D\u0438\u043A\u0438", "\u0416\u0438\u0432\u043E\u0442\u043D\u044B\u0435", "\u0413\u043E\u0442\u043E\u0432\u044B\u0439 \u0431\u0438\u0437\u043D\u0435\u0441 \u0438 \u043E\u0431\u043E\u0440\u0443\u0434\u043E\u0432\u0430\u043D\u0438\u0435", "\u0420\u0430\u0431\u043E\u0442\u0430", "\u0423\u0441\u043B\u0443\u0433\u0438", "\u041F\u0440\u043E\u0447\u0435\u0435"}));
 		
 		JComboBox<String> locationComboBox = new JComboBox<String>();
 		locationComboBox.setFont(new Font("Times New Roman", Font.PLAIN, 16));
 		locationComboBox.setFocusable(false);
-		locationComboBox.setModel(new DefaultComboBoxModel(new String[] {"\u0412\u0441\u044F \u0411\u0435\u043B\u0430\u0440\u0443\u0441\u044C", "\u0411\u0440\u0435\u0441\u0442\u0441\u043A\u0430\u044F \u043E\u0431\u043B.", "\u0412\u0438\u0442\u0435\u0431\u0441\u043A\u0430\u044F \u043E\u0431\u043B.", "\u0413\u043E\u043C\u0435\u043B\u044C\u0441\u043A\u0430\u044F \u043E\u0431\u043B.", "\u0413\u0440\u043E\u0434\u043D\u0435\u043D\u0441\u043A\u0430\u044F \u043E\u0431\u043B.", "\u041C\u043E\u0433\u0438\u043B\u0435\u0432\u0441\u043A\u0430\u044F \u043E\u0431\u043B.", "\u041C\u0438\u043D\u0441\u043A", "\u041C\u0438\u043D\u0441\u043A\u0430\u044F \u043E\u0431\u043B."}));
+		locationComboBox.setModel(new DefaultComboBoxModel<String>(new String[] {"\u0412\u0441\u044F \u0411\u0435\u043B\u0430\u0440\u0443\u0441\u044C", "\u0411\u0440\u0435\u0441\u0442\u0441\u043A\u0430\u044F \u043E\u0431\u043B.", "\u0412\u0438\u0442\u0435\u0431\u0441\u043A\u0430\u044F \u043E\u0431\u043B.", "\u0413\u043E\u043C\u0435\u043B\u044C\u0441\u043A\u0430\u044F \u043E\u0431\u043B.", "\u0413\u0440\u043E\u0434\u043D\u0435\u043D\u0441\u043A\u0430\u044F \u043E\u0431\u043B.", "\u041C\u043E\u0433\u0438\u043B\u0435\u0432\u0441\u043A\u0430\u044F \u043E\u0431\u043B.", "\u041C\u0438\u043D\u0441\u043A", "\u041C\u0438\u043D\u0441\u043A\u0430\u044F \u043E\u0431\u043B."}));
 		
 		JPanel centralPanel = new JPanel();
 		contentPane.add(centralPanel, BorderLayout.CENTER);
 		centralPanel.setLayout(new BorderLayout(0, 0));
+		
+		JTextArea textAreaLogger = new JTextArea();
+		textAreaLogger.setRows(3);
+		textAreaLogger.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+		textAreaLogger.setEditable(false);
+		
+		JScrollPane scrollPaneAreaLog = new JScrollPane(textAreaLogger);
+		scrollPaneAreaLog.setPreferredSize(new Dimension(1000, 65));
 		
 		JButton searchButton = new JButton("\u0418\u0441\u043A\u0430\u0442\u044C!");
 		searchButton.setFocusable(false);
 		searchButton.setFont(new Font("Times New Roman", Font.PLAIN, 16));
 		searchButton.addActionListener((e) -> {
 			
-		      DefaultTableModel tableModel = new DefaultTableModel();
-		      table = new JTable(tableModel);
-		      
-		      tableModel.addColumn("id");
-		      tableModel.addColumn("Имя товара");
-		      tableModel.addColumn("Цена");
-		      tableModel.addColumn("Место продажи");
-		      tableModel.addColumn("Продавец");
-		      tableModel.addColumn("Ссылка");
-		      
-		      String request = requestTextField.getText();
-		      String requestLink = "?query=" + request;	      
-		      
-		      if (!categoryComboBox.getSelectedItem().equals("Не выбрано")) {
-		    	  switch ((String) categoryComboBox.getSelectedItem()) {
-		    	      case ("Бытовая техника"): requestLink = "bytovaya-tehnika" + requestLink;
-		    	  break;
-		    	      case ("Компьютерная техника"): requestLink = "kompyuternaya-tehnika" + requestLink;
-		    	  break;
-		    	      case ("Телефоны и планшеты"): requestLink = "telefony-i-planshety" + requestLink;
-		    	  break;
-		    	      case ("Электроника"): requestLink = "elektronika" + requestLink;
-		    	  break;
-		    	      case ("Женский гардероб"): requestLink = "zhenskij-garderob" + requestLink;
-		    	  break;
-		    	      case ("Мужской гардероб"): requestLink = "muzhskoj-garderob" + requestLink;
-		    	  break;
-		    	      case ("Красота и здоровье"): requestLink = "krasota-i-zdorovie" + requestLink;
-		    	  break;
-		    	      case ("Всё для детей и мам"): requestLink = "vse-dlya-detej-i-mam" + requestLink;
-		    	  break;
-		    	      case ("Мебель"): requestLink = "mebel" + requestLink;
-		    	  break;
-		    	      case ("Все для дома"): requestLink = "vse-dlya-doma" + requestLink;
-		    	  break;
-		    	      case ("Ремонт и стройка"): requestLink = "remont-i-strojka" + requestLink;
-		    	  break;
-		    	      case ("Сад и огород"): requestLink = "sad-i-ogorod" + requestLink;
-		    	  break;
-		    	      case ("Хобби, спорт и туризм"): requestLink = "hobbi-sport-i-turizm" + requestLink;
-		    	  break;
-		    	      case ("Свадьба и праздники"): requestLink = "svadba-i-prazdniki" + requestLink;
-		    	  break;
-		    	      case ("Животные"): requestLink = "zhivotnye" + requestLink;
-		    	  break;
-		    	      case ("Готовый бизнес и оборудование"): requestLink = "gotovyj-biznes-i-oborudovanie" + requestLink;
-		    	  break;
-		    	      case ("Работа"): requestLink = "rabota-biznes-uchjoba" + requestLink;
-		    	  break;
-		    	      case ("Услуги"): requestLink = "uslugi" + requestLink;
-		    	  break;
-		    	      case ("Прочее"): requestLink = "prochee" + requestLink;
-		    	  break;
-		    	  }
-		      }
-		      
-		      if (!locationComboBox.getSelectedItem().equals("Вся Беларусь")) {
-		    	  switch ((String) locationComboBox.getSelectedItem()) {
-		    	      case ("Минск"): requestLink = "r~minsk/" + requestLink;
-		    	  break;
-		    	      case ("Брестская обл."): requestLink = "r~brestskaya-obl/" + requestLink;
-		    	  break;
-		    	      case ("Витебская обл."): requestLink = "r~vitebskaya-obl/" + requestLink;
-		    	  break;
-		    	      case ("Гомельская обл."): requestLink = "r~gomelskaya-obl/" + requestLink;
-		    	  break;
-		    	      case ("Гродненская обл."): requestLink = "r~grodnenskaya-obl/" + requestLink;
-		    	  break;
-		    	      case ("Могилевская обл."): requestLink = "r~mogilevskaya-obl/" + requestLink;
-		    	  break;
-		    	      case ("Минская обл."): requestLink = "r~minskaya-obl/" + requestLink;
-		    	  break;
-		    	  }
-		      }
-		      
-		      requestLink = "https://www.kufar.by/l/" + requestLink;
-		      
-		      ParseKufar parseKufar = new ParseKufar();
-		      String[] links = parseKufar.getLinks(requestLink);
-		      HashSet<String> linksToGoods = parseKufar.getGoods(links);
-		      ArrayList<HashMap<String, String>> information = parseKufar.getInfoFromGoods(linksToGoods);
-		      
-		      for (int i = 0; i < information.size(); i++) {
-		    	  tableModel.addRow(new Object[] {i, 
-		    			  						 information.get(i).get("itemName"), 
-		    			  						 information.get(i).get("itemPrice"), 
-		    			  						 information.get(i).get("sellerCity"), 
-		    			  						 information.get(i).get("sellerName"),
-		    			  						 information.get(i).get("itemLink")});
-		      }
-		      
-		      table.setModel(tableModel);
-		      table.getColumnModel().getColumn(0).setPreferredWidth(35);
-		      table.getColumnModel().getColumn(0).setMinWidth(25);
-		      table.getColumnModel().getColumn(0).setMaxWidth(50);
-		      table.getColumnModel().getColumn(1).setPreferredWidth(600);
-		      table.getColumnModel().getColumn(1).setMinWidth(300);
-		      table.getColumnModel().getColumn(1).setMaxWidth(800);
-		      table.getColumnModel().getColumn(2).setPreferredWidth(70);
-		      table.getColumnModel().getColumn(2).setMinWidth(50);
-		      table.getColumnModel().getColumn(2).setMaxWidth(100);
-		      table.getColumnModel().getColumn(3).setPreferredWidth(200);
-		      table.getColumnModel().getColumn(3).setMinWidth(100);
-		      table.getColumnModel().getColumn(3).setMaxWidth(400);
-		      table.getColumnModel().getColumn(4).setPreferredWidth(150);
-		      table.getColumnModel().getColumn(4).setMinWidth(80);
-		      table.getColumnModel().getColumn(4).setMaxWidth(200);
-		      table.getColumnModel().getColumn(5).setMinWidth(50);
-		      table.getColumnModel().getColumn(5).setMaxWidth(150);
-		      table.setFont(new Font("Times New Roman", Font.PLAIN, 16));
-		      table.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
-		      
-		      JScrollPane scrollPane = new JScrollPane(table);
-		      centralPanel.add(scrollPane);
-		      paintAll(getGraphics());
-		      
+			  Thread thread = new Thread() {
+				  
+				  public void run() {
+				      DefaultTableModel tableModel = new DefaultTableModel();
+				      table = new JTable(tableModel);
+				      
+				      tableModel.addColumn("id");
+				      tableModel.addColumn("Имя товара");
+				      tableModel.addColumn("Цена");
+				      tableModel.addColumn("Место продажи");
+				      tableModel.addColumn("Продавец");
+				      tableModel.addColumn("Ссылка");
+				      
+				      String request = requestTextField.getText();
+				      String requestLink = "?query=" + request;	      
+				      
+				      if (!categoryComboBox.getSelectedItem().equals("Не выбрано")) {
+				    	  switch ((String) categoryComboBox.getSelectedItem()) {
+				    	      case ("Бытовая техника"): requestLink = "bytovaya-tehnika" + requestLink;
+				    	  break;
+				    	      case ("Компьютерная техника"): requestLink = "kompyuternaya-tehnika" + requestLink;
+				    	  break;
+				    	      case ("Телефоны и планшеты"): requestLink = "telefony-i-planshety" + requestLink;
+				    	  break;
+				    	      case ("Электроника"): requestLink = "elektronika" + requestLink;
+				    	  break;
+				    	      case ("Женский гардероб"): requestLink = "zhenskij-garderob" + requestLink;
+				    	  break;
+				    	      case ("Мужской гардероб"): requestLink = "muzhskoj-garderob" + requestLink;
+				    	  break;
+				    	      case ("Красота и здоровье"): requestLink = "krasota-i-zdorovie" + requestLink;
+				    	  break;
+				    	      case ("Всё для детей и мам"): requestLink = "vse-dlya-detej-i-mam" + requestLink;
+				    	  break;
+				    	      case ("Мебель"): requestLink = "mebel" + requestLink;
+				    	  break;
+				    	      case ("Все для дома"): requestLink = "vse-dlya-doma" + requestLink;
+				    	  break;
+				    	      case ("Ремонт и стройка"): requestLink = "remont-i-strojka" + requestLink;
+				    	  break;
+				    	      case ("Сад и огород"): requestLink = "sad-i-ogorod" + requestLink;
+				    	  break;
+				    	      case ("Хобби, спорт и туризм"): requestLink = "hobbi-sport-i-turizm" + requestLink;
+				    	  break;
+				    	      case ("Свадьба и праздники"): requestLink = "svadba-i-prazdniki" + requestLink;
+				    	  break;
+				    	      case ("Животные"): requestLink = "zhivotnye" + requestLink;
+				    	  break;
+				    	      case ("Готовый бизнес и оборудование"): requestLink = "gotovyj-biznes-i-oborudovanie" + requestLink;
+				    	  break;
+				    	      case ("Работа"): requestLink = "rabota-biznes-uchjoba" + requestLink;
+				    	  break;
+				    	      case ("Услуги"): requestLink = "uslugi" + requestLink;
+				    	  break;
+				    	      case ("Прочее"): requestLink = "prochee" + requestLink;
+				    	  break;
+				    	  }
+				      }
+				      
+				      if (!locationComboBox.getSelectedItem().equals("Вся Беларусь")) {
+				    	  switch ((String) locationComboBox.getSelectedItem()) {
+				    	      case ("Минск"): requestLink = "r~minsk/" + requestLink;
+				    	  break;
+				    	      case ("Брестская обл."): requestLink = "r~brestskaya-obl/" + requestLink;
+				    	  break;
+				    	      case ("Витебская обл."): requestLink = "r~vitebskaya-obl/" + requestLink;
+				    	  break;
+				    	      case ("Гомельская обл."): requestLink = "r~gomelskaya-obl/" + requestLink;
+				    	  break;
+				    	      case ("Гродненская обл."): requestLink = "r~grodnenskaya-obl/" + requestLink;
+				    	  break;
+				    	      case ("Могилевская обл."): requestLink = "r~mogilevskaya-obl/" + requestLink;
+				    	  break;
+				    	      case ("Минская обл."): requestLink = "r~minskaya-obl/" + requestLink;
+				    	  break;
+				    	  }
+				      }
+				      
+				      requestLink = "https://www.kufar.by/l/" + requestLink;
+				      
+				      ParseKufar parseKufar = new ParseKufar();
+				      String[] links = parseKufar.getLinks(requestLink, textAreaLogger);
+				      HashSet<String> linksToGoods = parseKufar.getGoods(links, textAreaLogger);
+				      ArrayList<HashMap<String, String>> information = parseKufar.getInfoFromGoods(linksToGoods, textAreaLogger);
+				      
+				      for (int i = 0; i < information.size(); i++) {
+				    	  tableModel.addRow(new Object[] {i, 
+				    			  						 information.get(i).get("itemName"), 
+				    			  						 information.get(i).get("itemPrice"), 
+				    			  						 information.get(i).get("sellerCity"), 
+				    			  						 information.get(i).get("sellerName"),
+				    			  						 information.get(i).get("itemLink")});
+				      }
+				      
+				      table.setModel(tableModel);
+				      table.getColumnModel().getColumn(0).setPreferredWidth(35);
+				      table.getColumnModel().getColumn(0).setMinWidth(25);
+				      table.getColumnModel().getColumn(0).setMaxWidth(50);
+				      table.getColumnModel().getColumn(1).setPreferredWidth(600);
+				      table.getColumnModel().getColumn(1).setMinWidth(300);
+				      table.getColumnModel().getColumn(1).setMaxWidth(800);
+				      table.getColumnModel().getColumn(2).setPreferredWidth(70);
+				      table.getColumnModel().getColumn(2).setMinWidth(50);
+				      table.getColumnModel().getColumn(2).setMaxWidth(100);
+				      table.getColumnModel().getColumn(3).setPreferredWidth(200);
+				      table.getColumnModel().getColumn(3).setMinWidth(100);
+				      table.getColumnModel().getColumn(3).setMaxWidth(400);
+				      table.getColumnModel().getColumn(4).setPreferredWidth(150);
+				      table.getColumnModel().getColumn(4).setMinWidth(80);
+				      table.getColumnModel().getColumn(4).setMaxWidth(200);
+				      table.getColumnModel().getColumn(5).setMinWidth(50);
+				      table.getColumnModel().getColumn(5).setMaxWidth(150);
+				      table.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+				      table.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+				      
+				      JScrollPane scrollPane = new JScrollPane(table);
+				      centralPanel.add(scrollPane);
+				      paintAll(getGraphics());
+				      
+				  }
+				  
+			  }; 
+			  
+			  thread.start();
+			  checkIsRunning(thread, searchButton);
+
 		});
 		
 		infoPanel.add(Box.createRigidArea(new Dimension(70, 5)));
@@ -238,6 +257,43 @@ public class GUIParsing extends JFrame {
 		
 		Component rigidArea = Box.createRigidArea(new Dimension(10, 10));
 		emptyPanelBottom.add(rigidArea);
+		
+		JPanel BottomPanel = new JPanel();
+		contentPane.add(BottomPanel, BorderLayout.SOUTH);
+		BottomPanel.setLayout(new BorderLayout(0, 0));
+		
+		Component rigidArea_1 = Box.createRigidArea(new Dimension(200, 10));
+		BottomPanel.add(rigidArea_1, BorderLayout.WEST);
+		
+		Component rigidArea_1_1 = Box.createRigidArea(new Dimension(200, 10));
+		BottomPanel.add(rigidArea_1_1, BorderLayout.EAST);
+		
+		JPanel panelForLog = new JPanel();
+		panelForLog.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
+		BottomPanel.add(panelForLog, BorderLayout.CENTER);
+		panelForLog.setLayout(new BorderLayout(0, 0));
+		
+		panelForLog.add(scrollPaneAreaLog);
+		
+	}
+	
+	//Отключаем кнопку поиска до тех пор, пока он не завершится
+	public void checkIsRunning(Thread thread, JButton button) {
+		
+		Thread checkThread = new Thread() {
+			
+			public void run() {
+				while (thread.isAlive()) {
+					  button.setEnabled(false);
+					  button.setToolTipText("Подождите пока закончится поиск!");
+				}
+				  button.setEnabled(true);
+				  button.setToolTipText(null);
+			}
+			
+		};
+		
+		checkThread.start();
 		
 	}
 
--- src/parsing/ParseKufar.java
+++ src/parsing/ParseKufar.java
@@ -7,11 +7,15 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.swing.JTextArea;
+
+import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.openqa.selenium.By;
+import org.openqa.selenium.ElementClickInterceptedException;
 import org.openqa.selenium.ElementNotInteractableException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
@@ -19,21 +23,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ParseKufar {
-
-	public static void main(String[] args) {
-		
-		ParseKufar parseKufar = new ParseKufar();
-		String[] links = parseKufar.getLinks("https://www.kufar.by/l/r~pinsk/velotovary?query=%D0%B2%D0%B5%D0%BB%D0%BE%D1%81%D0%B8%D0%BF%D0%B5%D0%B4&sort=lst.d");
-		HashSet<String> linksToGoods = parseKufar.getGoods(links);
-//		HashSet<String> testLinks = new HashSet<String>();
-//		testLinks.add("https://www.kufar.by/item/171343513");
-		parseKufar.getInfoFromGoods(linksToGoods);
-		
-	}
 	
-	public String[] getLinks(String URL) {
+	private String[] links;
+	
+	public String[] getLinks(String URL, JTextArea textArea) {
 		
-		System.setProperty("webdriver.chrome.driver", "selenium\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\selenium\\chromedriver.exe");
 		WebDriver webDriver = new ChromeDriver();
 		webDriver.get(URL);
 		
@@ -48,29 +43,35 @@ public class ParseKufar {
 		} catch (NoSuchElementException e) { //Элемент рекламы не успел загрузиться, ждем
 			try {
 				System.out.println("No such element and you have to wait!");
+				textArea.setText(textArea.getText() + "\n No such element and you have to wait!");
 				Thread.sleep(500);
 				popupWindows = webDriver.findElement(By.xpath("//*[@id=\"portal\"]/div/div[2]/div[1]/div/button"));
 				popupWindows.click();
 				System.out.println("Sucsess!");
+				textArea.setText(textArea.getText() + "\n Sucsess!");
 			} catch (InterruptedException e1) {
 				e1.printStackTrace();
 			} catch (NoSuchElementException e1) { //Рекламу убрали с сайта
 				System.out.println("Oops, the ad is not found!");
+				textArea.setText(textArea.getText() + "\n Oops, the ad is not found!");
 			}
 		} catch (ElementNotInteractableException e) { //Неудача при первой попытке закрыть рекламу
 			System.out.println("One more try to click!");
+			textArea.setText(textArea.getText() + "\n One more try to click!");
 			popupWindows = webDriver.findElement(By.xpath("//*[@id=\"portal\"]/div/div[2]/div[1]/div/img"));
 			popupWindows.click();
 			System.out.println("Sucsess!");
+			textArea.setText(textArea.getText() + "\n Sucsess!");
 		}
 		
 		try {
 			//Поиск количества страниц и создание массива для их хранения
 			List<WebElement> pages = webDriver.findElements(By.className("styles_link__KajLs"));
 			int lastPage = Integer.parseInt(pages.get(pages.size() - 2).getText());
-			String[] links = new String[lastPage];
+			links = new String[lastPage];
 			
 			System.out.println("There are " + lastPage + " pages. Starting to get links.");
+			textArea.setText(textArea.getText() + "\n There are " + lastPage + " pages. Starting to get links.");
 //			int n = 1;
 			for (int i = 0; i < lastPage; i++) {
 				links[i] = webDriver.getCurrentUrl();
@@ -98,6 +99,7 @@ public class ParseKufar {
 			}
 			
 			System.out.println("Links added.");
+			textArea.setText(textArea.getText() + "\n Links added.");
 			webDriver.quit();
 			return links;
 
@@ -108,22 +110,29 @@ public class ParseKufar {
 //			}
 			
 		} catch (IndexOutOfBoundsException e) { //Только 1 страница с товарами
-			String[] links = new String[1];
+			links = new String[1];
 			links[0] = webDriver.getCurrentUrl();
+			textArea.setText(textArea.getText() + "\n Links added.");
 			System.out.println("Links added.");
+			webDriver.quit();
+			return links;
+		} catch (ElementClickInterceptedException e) {
+			System.out.println("Advertising breaks plans, we take what we have.");
+			textArea.setText(textArea.getText() + "\n Advertising breaks plans, we take what we have.");
 			webDriver.quit();
 			return links;
 		}
 	
 	}
 
-	public HashSet<String> getGoods(String[] links) {
+	public HashSet<String> getGoods(String[] links, JTextArea textArea) {
 		
 		Set<String> linksToGoods = new HashSet<String>(); //Хранение ссылок на товары
 		Document document; 
 		Elements goods;
 		
 		System.out.println("Starting to get goods.");
+		textArea.setText(textArea.getText() + "\n Starting to get goods.");
 		for (int i = 0; i < links.length; i++) {
 			try {
 				document = Jsoup.connect(links[i]).get();
@@ -137,6 +146,7 @@ public class ParseKufar {
 		}
 		
 		System.out.println("Finish. There are " + linksToGoods.size() + " goods.");
+		textArea.setText(textArea.getText() + "\n Finish. There are " + linksToGoods.size() + " goods.");
 		return (HashSet<String>) linksToGoods;
 		
 //		int k = 1;
@@ -146,46 +156,72 @@ public class ParseKufar {
 	
 	}
 	
-	public ArrayList<HashMap<String, String>> getInfoFromGoods(HashSet<String> linksToGoods) {
+	public ArrayList<HashMap<String, String>> getInfoFromGoods(HashSet<String> linksToGoods, JTextArea textArea) {
 		
 		List<HashMap<String, String>> information = new ArrayList<HashMap<String, String>>();
 		Document document;
 		
 		System.out.println("Starting to get info from goods.");
+		textArea.setText(textArea.getText() + "\n Starting to get info from goods.");
 		for (int i = 0; i < linksToGoods.size(); i++) {
 			
+			//Подсчет количества пройденных товаров
+			if (i % 50 == 0) {
+				System.out.println("We're on " + i + " item!");
+				textArea.setText(textArea.getText() + "\n We're on " + i + " item!");
+			}
+
 			HashMap<String, String> tempHashMap = new HashMap<String,String>();
 			try {
 				document = Jsoup.connect((String) linksToGoods.toArray()[i]).get();
 				Element element;
 				
 				//Берем со страницы имя товара
-				element = document.selectFirst("h1.styles_brief_wrapper__title__x59rm");
-				tempHashMap.put("itemName", element.text());
-				
+				try {
+					element = document.selectFirst("h1.styles_brief_wrapper__title__x59rm");
+					tempHashMap.put("itemName", element.text());
+				} catch (NullPointerException e) {
+					tempHashMap.put("itemName", "Имя не найдено!");
+				}
+
 				//Берем со страницы цену товара
-				element = document.selectFirst("span.styles_main__PU1v4");
-				tempHashMap.put("itemPrice", element.text());
-				
+				try {
+					element = document.selectFirst("span.styles_main__PU1v4");
+					tempHashMap.put("itemPrice", element.text());
+				} catch (NullPointerException e) {
+					tempHashMap.put("itemPrice", "Цена не найдена!");
+				}
+
 				//Берем имя продавца товара
-				element = document.selectFirst("div.styles_seller-block__top-right-name__imyGc");
-				tempHashMap.put("sellerName", element.text());
-				
+				try {
+					element = document.selectFirst("div.styles_seller-block__top-right-name__imyGc");
+					tempHashMap.put("sellerName", element.text());
+				} catch (NullPointerException e) {
+					tempHashMap.put("sellerName", "Имя не найдено!");
+				}
+
 				//Берем город продажи
-				element = document.selectFirst("span.styles_address__fQyGA");
-				tempHashMap.put("sellerCity", element.text());
-				
+				try {
+					element = document.selectFirst("span.styles_address__fQyGA");
+					tempHashMap.put("sellerCity", element.text());
+				} catch (NullPointerException e) {
+					tempHashMap.put("sellerCity", "Город не найден!");
+				}
+
 				//Берем ссылку на товар
 				tempHashMap.put("itemLink", (String) linksToGoods.toArray()[i]);
 				
-				
 				information.add(tempHashMap);
+			} catch (HttpStatusException e) {
+				System.out.println(e.getUrl() + " error fetching URL");
+				textArea.setText(textArea.getText() + "\n error fetching URL");
 			} catch (IOException e) {
 				e.printStackTrace();
-			}
+			} 
 				
 		}
-		System.out.println("Finish.");
+		System.out.println("Done.");
+		textArea.setText(textArea.getText() + "\n Done.");
 		return (ArrayList<HashMap<String, String>>) information;
 		
 //		int k = 1;
